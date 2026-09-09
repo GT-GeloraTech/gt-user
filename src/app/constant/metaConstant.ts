6478839c03d9e8025cd382b7fc87aaddb8df1889
Naga-GeloraTech <naga.vishnu@geloratech.com>
2026-09-09 19:09:11 +0530

    base setup done

---
+++ src/app/constant/metaConstant.ts
@@ -0,0 +1,8 @@
+export const PAGE_TITLES = {
+	HOME: "Home | Gelora Tech",
+	ABOUT: "About | Gelora Tech",
+	CAREERS: "Careers | Gelora Tech",
+	PRODUCT: "Product | Gelora Tech",
+	PROJECTS: "Projects | Gelora Tech",
+	SERVICES: "Services | Gelora Tech",
+} as const;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import { PAGE_TITLES } from "./Constant/metaConstant";
+
+export const metadata: Metadata = {
+  title: PAGE_TITLES.HOME,
+};
+
+export default function Home() {
+  return (
+     <>
+     
+     </>
+  );
+}
